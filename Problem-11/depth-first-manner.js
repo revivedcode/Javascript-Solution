33906bf15d4f96c99f1726b1b790923b0fc84760
Front End Developer <revivedcode@gmail.com>
2025-06-19 21:30:27 +0530

    depth first manner js coding upload with solution

---
+++ Problem-11/depth-first-manner.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+        id: "1",
+        name: "CEO",
+        children: [
+            {
+                id: "2",
+                name: "CTO",
+                children: [
+                    {
+                        id: "5",
+                        name: "Dev Manager",
+                        children: [
+                            { id: "9", name: "Frontend Dev", children: [] },
+                            { id: "10", name: "Backend Dev", children: [] }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "3",
+                name: "CFO",
+                children: [
+                    {
+                        id: "6",
+                        name: "Account Manager",
+                        children: [
+                            { id: "11", name: "Accountant", children: [] }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "4",
+                name: "COO",
+                children: [
+                    {
+                        id: "7",
+                        name: "Operations Lead",
+                        children: [
+                            {
+                                id: "12",
+                                name: "Logistics Manager",
+                                children: []
+                            }
+                        ]
+                    },
+                    {
+                        id: "8",
+                        name: "Support Lead",
+                        children: []
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+function printIndented(data, level = 0) {
+    for (const item of data) {
+        console.log(' '.repeat(level) + item.name);
+        if (item.children && item.children.length > 0) {
+            printIndented(item.children, level + 1)
+        }
+    }
+}
+
+printIndented(data);
